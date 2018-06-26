21a0e4942ae790be286915d9e53fce98fc779d98
ben-vandelaar <benvandelaar@gmail.com>
2018-06-26 15:13:55 +1200

    added counter

--- game.js
+++ game.js
@@ -42,7 +42,16 @@ function updateCounts () {
   }
   
   // WRITE CODE HERE TO COUNT BLUE, GREEN, AND INVISIBLE DOTS
-
+  var totalDots = document.getElementsByClassName('board')[0].children;
+  for (i = 0; i < totalDots.length; i++){
+    if (totalDots[i].classList.contains('blue')){
+      totals.blue++
+    }else if (totalDots[i].classList.contains('green')){
+        totals.green++
+    }else if(totalDots[i].classList.contains('invisible')){
+      totals.invisible++
+    }
+  }
   // Once you've done the counting, this function will update the display
   displayTotals(totals)
 }
